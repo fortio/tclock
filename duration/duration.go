cbdcb46d6900d8264fb57270270d89a94c19782e
Laurent Demailly <ldemailly@gmail.com>
2025-09-11 11:35:40 -0700

    fix deprecation comments

--- duration/duration.go
+++ duration/duration.go
@@ -1,4 +1,5 @@
 // Package duration allows duration parsing with "d" for days (24 hours) and "w" for week (7 days).
+//
 // Deprecated: package has moved to standalone 0 dependencies [fortio.org/duration].
 package duration
 
@@ -9,12 +10,15 @@ import (
 )
 
 const (
-	Day  = 24 * time.Hour
-	Week = 7 * Day
+	// Deprecated: use [fortio.org/duration/Day] directly.
+	Day = duration.Day
+	// Deprecated: use [fortio.org/duration/Week] directly.
+	Week = duration.Week
 )
 
 // Parse parses a duration string with "d" for days (24 hours)
 // and "w" for weeks (7 days) in addition to what stdlib [time.ParseDuration] supports.
+//
 // Deprecated: use [fortio.org/duration/Parse] directly.
 func Parse(s string) (time.Duration, error) {
 	return duration.Parse(s)
@@ -26,6 +30,7 @@ type Duration = duration.Duration
 // Flag defines a duration flag with the specified name, default value, and usage string, like
 // [flag.Duration] but supporting durations in days (24 hours) and weeks (7 days)
 // in addition to the other stdlib units.
+//
 // Deprecated: use [fortio.org/duration/Flag] directly.
 func Flag(name string, value time.Duration, usage string) *time.Duration {
 	return duration.Flag(name, value, usage)
@@ -36,6 +41,7 @@ func Flag(name string, value time.Duration, usage string) *time.Duration {
 // in the future around daylight savings transitions. If it's a double 3a-4a transition, and now is the
 // first 3:10am asking for 3:05a will not give the very next 3:05a (same day) but instead the one next day
 // ie at minimum 23h later. The timezone of now is the one used and timezone of d should be the same.
+//
 // Deprecated: use [fortio.org/duration/NextTime].
 func NextTime(now, d time.Time) time.Time {
 	return duration.NextTime(now, d)
@@ -54,6 +60,7 @@ var ErrDateTimeParsing = duration.ErrDateTimeParsing
 // When the time is missing 00:00 is assumed.
 // The time/date is interpreted in relation to the location and timezone of the `now` parameter (local time
 // per TZ environment when using time.Now() for instance).
+//
 // Deprecated: use [fortio.org/duration/ParseDateTime] directly.
 func ParseDateTime(now time.Time, s string) (time.Time, error) {
 	return duration.ParseDateTime(now, s)
